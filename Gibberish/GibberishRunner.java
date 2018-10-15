2aeac849ab7fa92ee3bd8b496225e3cdd41eb53b
797077 <42552641+797077@users.noreply.github.com>
2018-10-15 10:09:37 -0700

    Gibberish Project; commented throughout

--- Gibberish/GibberishRunner.java
+++ Gibberish/GibberishRunner.java
@@ -7,8 +7,8 @@
  */
 public class GibberishRunner{
     public static void main(){
-        WordList5000 wordList5000 = new WordList5000();
-        Gibberish gibberish = new Gibberish();
-        System.out.println(gibberish.makeGibberish(wordList5000));
+        WordList5000 wordList5000 = new WordList5000();//makes new WordList5000
+        Gibberish gibberish = new Gibberish();//makes Gibberish object
+        System.out.println(gibberish.makeGibberish(wordList5000));//runs makeGibberish method and prints returned String
     }
 }
